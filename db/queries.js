191f39b6612fbf8be3a2e980d41ebe7b426e6df4
Pawel <none>
2025-03-01 11:42:10 +0100

    db name change

--- db/queries.js
+++ db/queries.js
@@ -3,7 +3,7 @@ import { pool } from "../db/pool.js";
 export default function db() {}
 
 export async function getAll() {
-  const { rows } = await pool.query("SELECT * FROM mini-message-board");
+  const { rows } = await pool.query("SELECT * FROM usernames");
   return rows;
 }
 
